4c301eae469db9e6d308feb3f4a4951fb6c92e2f
Arindam <arindam.datta@decos.com>
2021-02-19 20:11:37 +0530

    removed stock from product

--- src/product/product.js
+++ src/product/product.js
@@ -1,12 +1,11 @@
 class Product {
-    constructor(id, type, name, version, color, seller, stock) {
+    constructor(id, type, name, version, color, seller) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
         this.color = color;
         this.seller = seller;
-        this.stock = stock;
 
     }
 }
--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -40,7 +40,7 @@ describe("Pact Verification", () => {
     const stateHandlers = {
       "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red", "paradise","yes")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red", "paradise")]
         ]);
       },
       "a product with ID 11 does not exist": () => {

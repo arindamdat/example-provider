b0c635ba017b41e1649e8918a64afe3f189b02e5
Arindam <arindam.datta@decos.com>
2021-02-19 19:49:02 +0530

    added seller and stock property

--- src/product/product.js
+++ src/product/product.js
@@ -1,10 +1,13 @@
 class Product {
-    constructor(id, type, name, version, color) {
+    constructor(id, type, name, version, color, seller, stock) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
         this.color = color;
+        this.seller = seller;
+        this.stock = stock;
+
     }
 }
 
--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -4,9 +4,9 @@ class ProductRepository {
 
     constructor() {
         this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green")],
-            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red")],
-            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "blue")],
+            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green", "paradise","yes")],
+            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red", "paradise","yes")],
+            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "blue", "skyshop","no")],
         ]);
     }
 
